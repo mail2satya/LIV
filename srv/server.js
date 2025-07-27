a0590295d34a8b6d52f52a850d4d6e4aa189f40f
Satyabrata Padhan <mail2satya.padhan@gmail.com>
2025-07-27 15:00:18 +0000

    Initial commit

--- srv/server.js
+++ srv/server.js
@@ -1,4 +1,7 @@
 const cds = require('@sap/cds');
+const proxy = require('@sap/cds-odata-v2-adapter-proxy')
+cds.on('bootstrap', app => app.use(proxy()))
+module.exports = cds.server
 
 module.exports = cds.service.impl('LivService', async function() {
     const { Orders, Devices, Customers } = this.entities;
